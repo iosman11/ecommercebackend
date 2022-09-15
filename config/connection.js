99448e4f5533a9b1afa9f469d5bc87cecd7da016
idil osman <idil_abukar@live.se>
2022-09-15 19:40:34 +0100

    added connections

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// Import:
+// Sequelize:
+const Sequelize = require("sequelize");
+
+// Dotenv:
+require("dotenv").config();
+
+// Establish a connection:
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+// Export:
+module.exports = sequelize;
